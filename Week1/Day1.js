93a2f96dfb7b0b5cd0c827c437002c04908b091c
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2025-03-24 10:19:15 +0200

    Class assignment

---
+++ Week1/Day1.js
@@ -0,0 +1,9 @@
+let x = 5;
+let y = 8;
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+
+console.log(`The sum is ${sum}, The difference is ${difference}`);
+console.log(`The product is ${product}, The quotient is ${quotient}`);
